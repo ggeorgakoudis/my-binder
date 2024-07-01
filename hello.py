1c851f6bd8cfe29f2d8bd288b703290ef2e26793
Giorgis Georgakoudis <ggeorgakoudis@gmail.com>
2024-07-01 12:03:36 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello from Binder!")
+from numba import njit
+from numba.openmp import openmp_context as openmp
+from numba.openmp import omp_get_num_threads, omp_get_num_devices, omp_get_thread_num, omp_get_team_num, omp_get_num_teams
+import numpy as np
+@njit()
+def test():
+    with openmp("parallel"):
+        print("Hello from thread", omp_get_thread_num(), "/", omp_get_num_threads())
+      
+test()
+
+print("DONE")
